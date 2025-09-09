addc10e5fcc5965d94a5179baa8b4be66bf6fffe
Deadchicken07 <deadchicken07@gmail.com>
2025-09-09 23:26:38 +0700

    Tect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps): React.JSX.Element {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body>
         <LocalizationProvider>
           <UserProvider>
